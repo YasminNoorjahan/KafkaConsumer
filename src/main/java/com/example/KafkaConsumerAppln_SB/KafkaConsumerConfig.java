ed09ff6fb16f2a2aa3c44a3b60f06082ba3bd8c0
Noorjahan <noorjahann1996@gmail.com>
2023-12-12 11:35:55 +0530

    SB_Kafka integration

--- src/main/java/com/example/KafkaConsumerAppln_SB/KafkaConsumerConfig.java
+++ src/main/java/com/example/KafkaConsumerAppln_SB/KafkaConsumerConfig.java
@@ -61,6 +61,7 @@ public class KafkaConsumerConfig {
         Employee employee = (Employee) ois.readObject();
         ois.close();
         bais.close();*/
+
         System.out.println("Received Message in group test: " + message);
     }
 }
